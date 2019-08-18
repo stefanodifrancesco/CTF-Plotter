61740bb029f6a3727e5fa5fd77b37a337634f958
Stefano Di Francesco <8914928+stefanodifrancesco@users.noreply.github.com>
2019-08-18 20:05:14 +0200

    Update source code of the plugin

--- org.eclipse.tracecompass.ctf.plotter/src/org/eclipse/tracecompass/ctf/plotter/RTplotter.java
+++ org.eclipse.tracecompass.ctf.plotter/src/org/eclipse/tracecompass/ctf/plotter/RTplotter.java
@@ -16,9 +16,19 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swtchart.Chart;
+import org.eclipse.swtchart.ILineSeries;
+import org.eclipse.swtchart.ISeries.SeriesType;
+import org.eclipse.swtchart.LineStyle;
+import org.eclipse.swtchart.Range;
+import org.eclipse.swtchart.extensions.charts.InteractiveChart;
 import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
 import org.eclipse.tracecompass.tmf.core.event.ITmfEventField;
 import org.eclipse.tracecompass.tmf.core.event.TmfEvent;
@@ -29,24 +39,38 @@ import org.eclipse.tracecompass.tmf.core.request.TmfEventRequest;
 import org.eclipse.tracecompass.tmf.core.timestamp.TmfTimeRange;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 import org.eclipse.tracecompass.tmf.ui.project.model.ITmfProjectModelElement;
+import org.eclipse.tracecompass.tmf.ui.project.model.TmfProjectElement;
 import org.eclipse.tracecompass.tmf.ui.project.model.TmfProjectRegistry;
 import org.eclipse.tracecompass.tmf.ui.project.model.TmfTraceElement;
 import org.eclipse.tracecompass.tmf.ui.project.model.TmfTraceFolder;
 import org.eclipse.tracecompass.tmf.ui.views.TmfView;
-import org.swtchart.Chart;
-import org.swtchart.ILineSeries;
-import org.swtchart.ISeries.SeriesType;
-import org.swtchart.LineStyle;
-import org.swtchart.Range;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 public class RTplotter extends TmfView {
 
 	private static final String SERIES_NAME = "Series";
 	private static final String Y_AXIS_TITLE = "Timestamp";
 	private static final String X_AXIS_TITLE = "ID";
-	private static final String FIELD = "timestamp_field"; // The name of the field that we want to display on the Y axis
+	private static final String FIELD = "timestamp_field"; // The name of the field that we want to display on the Y
+															// axis
 	private static final String VIEW_ID = "org.eclipse.tracecompass.ctf.plotter.RTplot";
+	
+	IProject project = null;
+	private String projectName;
 	private Chart chart;
+	Job job;
+
+	Action startDrawing;
+	Action stopDrawing;
+	Action adjustRange;
+	Action invertAxis;
+	Action clearChart;
 
 	IWorkspace workspace = ResourcesPlugin.getWorkspace();
 	IResourceChangeListener listener;
@@ -57,76 +81,208 @@ public class RTplotter extends TmfView {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		chart = new Chart(parent, SWT.BORDER);
-		chart.getTitle().setVisible(false);
+		chart = new InteractiveChart(parent, SWT.NONE);
+
+		// Set axis names
 		chart.getAxisSet().getXAxis(0).getTitle().setText(X_AXIS_TITLE);
 		chart.getAxisSet().getYAxis(0).getTitle().setText(Y_AXIS_TITLE);
+		// Set axis default ranges
+		chart.getAxisSet().getXAxis(0).setRange(new Range(1, 4294967295d));
+		chart.getAxisSet().getYAxis(0).setRange(new Range(100000, 1000000));
+		// Set invisible line
 		ILineSeries series = (ILineSeries) chart.getSeriesSet().createSeries(SeriesType.LINE, SERIES_NAME);
 		series.setLineStyle(LineStyle.NONE);
 		chart.getLegend().setVisible(false);
 
+		// Get workspace view containing tracing projects
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		IViewPart viewPart = page.findView(IPageLayout.ID_PROJECT_EXPLORER);
+		ISelectionProvider selProvider = viewPart.getSite().getSelectionProvider();
 		
-		Job job = new Job("Plotting") {
+		// Create a new job for plotting at intervals
+		job = new Job("Plotting") {
 			protected IStatus run(IProgressMonitor monitor) {
 				try {
-					IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("Tracing");
-					TmfTraceFolder destinationFolder = TmfProjectRegistry.getProject(project, true).getTracesFolder();
-					System.out.println(destinationFolder);
-						
-					final String pathToUse = checkTracePath("/home/ctf");
-					IFolder folder = destinationFolder.getResource();
-					String traceName = getTraceName(pathToUse, folder);
-
-					final List<ITmfProjectModelElement> elements = destinationFolder.getChildren();
 					
-					TmfTraceElement traceElement = null;
-					for (ITmfProjectModelElement element : elements) {
-						if (element instanceof TmfTraceElement && element.getName().equals(traceName)) {
-							traceElement = (TmfTraceElement) element;
+					// Get selected tracing project from the workspace
+					ISelection selection = selProvider.getSelection();
+					IResource res = null;
+					if (selection instanceof IStructuredSelection) {
+						IStructuredSelection ss = (IStructuredSelection) selection;
+						Object element = ss.getFirstElement();
+						if (element instanceof IResource)
+							res = (IResource) element;
+					}
+					
+					if (res != null) {
+						project = res.getProject();
+						
+						/*********** This piece of code has been adapted from TraceCompass sources **********/
+						TmfProjectElement tmfProject = TmfProjectRegistry.getProject(project, true);
+						projectName = tmfProject.getName();
+						TmfTraceFolder destinationFolder = tmfProject.getTracesFolder();
+						if (destinationFolder.getChildren().size() > 0) {
+							
+							String ctf_absolute_path = destinationFolder.getChildren().get(0).getLocation().getPath();
+							
+							final String pathToUse = checkTracePath(ctf_absolute_path);
+							IFolder folder = destinationFolder.getResource();
+							String traceName = getTraceName(pathToUse, folder);
+							
+							final List<ITmfProjectModelElement> elements = destinationFolder.getChildren();
+							
+							TmfTraceElement traceElement = null;
+							for (ITmfProjectModelElement element : elements) {
+								if (element instanceof TmfTraceElement && element.getName().equals(traceName)) {
+									traceElement = (TmfTraceElement) element;
+								}
+							}
+
+							final ITmfTrace trace = traceElement.instantiateTrace();
+							final ITmfEvent traceEvent = traceElement.instantiateEvent();
+							
+							try {
+								trace.initTrace(traceElement.getResource(),
+										traceElement.getResource().getLocation().toOSString(), traceEvent.getClass(),
+										traceElement.getElementPath(), traceElement.getTraceType());
+							} catch (TmfTraceException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							/*************************************************************************************/
+							
+							
+							// Run on the UI thread the code to render the CTF of the selected tracing project
+							Display.getDefault().asyncExec(new Runnable() {
+								@Override
+								public void run() {
+									chart.getTitle().setText("Chart of " + projectName);
+									drawTraceSelected(trace);
+								}
+							});
+
+							// Wait the trace is rendered
+							try {
+								Thread.sleep(200);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							// then dispose
+							trace.dispose();
+						} else {
+							Display.getDefault().asyncExec(new Runnable() {
+								@Override
+								public void run() {
+									chart.getTitle().setText("No chart for empty project");
+									final double x[] = new double[0];
+									final double y[] = new double[0];
+									chart.getSeriesSet().getSeries()[0].setXSeries(x);
+									chart.getSeriesSet().getSeries()[0].setYSeries(y);
+									chart.redraw();
+								}
+							});
 						}
+						
+					} else {
+						if (project == null) {
+							Display.getDefault().asyncExec(new Runnable() {
+								@Override
+								public void run() {
+									chart.getTitle().setText("No chart for empty project");
+									final double x[] = new double[0];
+									final double y[] = new double[0];
+									chart.getSeriesSet().getSeries()[0].setXSeries(x);
+									chart.getSeriesSet().getSeries()[0].setYSeries(y);
+									chart.redraw();
+								}
+							});
+						}						
 					}
-
-					final ITmfTrace trace = traceElement.instantiateTrace();
-					final ITmfEvent traceEvent = traceElement.instantiateEvent();
-
-					try {
-						trace.initTrace(traceElement.getResource(),
-								traceElement.getResource().getLocation().toOSString(), traceEvent.getClass(),
-								traceElement.getElementPath(), traceElement.getTraceType());
-					} catch (TmfTraceException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					Display.getDefault().asyncExec(new Runnable() {
-						@Override
-						public void run() {
-							drawTraceSelected(trace);
-						}
-					});
-
-					// Wait the trace is analyzed
-					try {
-						Thread.sleep(200);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					// then dispose
-					trace.dispose();
-
+					
 					if (monitor.isCanceled())
 						return Status.CANCEL_STATUS;
 
 					return Status.OK_STATUS;
 				} finally {
-					schedule(1000); // the job start again in 500 millisecond
+					schedule(1000); // the job start again in 1000 millisecond. Too low interval will cause unresponsive UI.
 				}
 			}
 		};
 		job.setPriority(Job.SHORT);
-		job.schedule(); // start as soon as possible
-       
+		// job.schedule(); // start as soon as possible
+
+		startDrawing = new Action() {
+			public void run() {
+				if (job.getState() == Job.NONE) {
+					job.schedule();
+				}
+			}
+		};
+		startDrawing.setText("start");
+		startDrawing.setToolTipText("Start drawing");
+		startDrawing.setImageDescriptor(
+				PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_OPEN_MARKER));
+
+		stopDrawing = new Action() {
+			public void run() {
+				while (job.getState() == Job.RUNNING) {
+
+				}
+				job.cancel();
+			}
+		};
+		stopDrawing.setText("stop");
+		stopDrawing.setToolTipText("Stop drawing");
+		stopDrawing.setImageDescriptor(
+				PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_ELCL_STOP));
+
+		adjustRange = new Action() {
+			public void run() {
+				chart.getAxisSet().getXAxis(0).setRange(new Range(1, 4294967295d));
+				chart.getAxisSet().getYAxis(0).setRange(new Range(100000, 1000000));
+			}
+		};
+		adjustRange.setText("Adjust");
+		adjustRange.setToolTipText("Adjust range");
+		adjustRange.setImageDescriptor(
+				AbstractUIPlugin.imageDescriptorFromPlugin("org.eclipse.tracecompass.ctf.plotter", "icons/adjust.jpg"));
+
+		invertAxis = new Action() {
+			public void run() {
+				if (chart.getOrientation() == SWT.HORIZONTAL) {
+					chart.setOrientation(SWT.VERTICAL);
+				} else {
+					chart.setOrientation(SWT.HORIZONTAL);
+				}
+
+			}
+		};
+		invertAxis.setText("Invert");
+		invertAxis.setToolTipText("Invert axis");
+		invertAxis.setImageDescriptor(
+				PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_ELCL_SYNCED));
+		
+		clearChart = new Action() {
+			public void run() {
+				final double x[] = new double[0];
+				final double y[] = new double[0];
+				chart.getSeriesSet().getSeries()[0].setXSeries(x);
+				chart.getSeriesSet().getSeries()[0].setYSeries(y);
+				chart.redraw();
+			}
+		};
+		clearChart.setText("clear");
+		clearChart.setToolTipText("Clear");
+		clearChart.setImageDescriptor(
+				PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_ETOOL_CLEAR));
+
+		IActionBars bars = getViewSite().getActionBars();
+		bars.getToolBarManager().add(startDrawing);
+		bars.getToolBarManager().add(stopDrawing);
+		bars.getToolBarManager().add(adjustRange);
+		bars.getToolBarManager().add(invertAxis);
+		bars.getToolBarManager().add(clearChart);
 	}
 
 	private String checkTracePath(String path) {
@@ -188,10 +344,10 @@ public class RTplotter extends TmfView {
 
 			ArrayList<Double> xValues = new ArrayList<Double>();
 			ArrayList<Double> yValues = new ArrayList<Double>();
-			private double maxY = -Double.MAX_VALUE;
-			private double minY = Double.MAX_VALUE;
-			private double maxX = -Double.MAX_VALUE;
-			private double minX = Double.MAX_VALUE;
+//			private double maxY = -Double.MAX_VALUE;
+//			private double minY = Double.MAX_VALUE;
+//			private double maxX = -Double.MAX_VALUE;
+//			private double minX = Double.MAX_VALUE;
 
 			@Override
 			public void handleData(ITmfEvent data) {
@@ -201,14 +357,14 @@ public class RTplotter extends TmfView {
 				ITmfEventField field = data.getContent().getField(FIELD);
 				if (field != null) {
 					Double yValue = ((Long) field.getValue()).doubleValue();
-					minY = Math.min(minY, yValue);
-					maxY = Math.max(maxY, yValue);
+//					minY = Math.min(minY, yValue);
+//					maxY = Math.max(maxY, yValue);
 					yValues.add(yValue);
 
 					double xValue = ((Long) data.getContent().getField("ID_field").getValue()).doubleValue();
 					xValues.add(xValue);
-					minX = Math.min(minX, xValue);
-					maxX = Math.max(maxX, xValue);
+//					minX = Math.min(minX, xValue);
+//					maxX = Math.max(maxX, xValue);
 				}
 			}
 
@@ -217,8 +373,6 @@ public class RTplotter extends TmfView {
 				// Request successful, not more data available
 				super.handleSuccess();
 
-				System.out.println(xValues.size());
-				System.out.println(yValues.size());
 				final double x[] = toArray(xValues);
 				final double y[] = toArray(yValues);
 
@@ -232,15 +386,13 @@ public class RTplotter extends TmfView {
 						chart.getSeriesSet().getSeries()[0].setYSeries(y);
 
 						// Set the new range
-						if (!xValues.isEmpty() && !yValues.isEmpty()) {
-							chart.getAxisSet().getXAxis(0).setRange(new Range(0, x[x.length - 1]));
-							chart.getAxisSet().getYAxis(0).setRange(new Range(minY, maxY));
-						} else {
-							chart.getAxisSet().getXAxis(0).setRange(new Range(0, 1));
-							chart.getAxisSet().getYAxis(0).setRange(new Range(0, 1));
-						}
-						chart.getAxisSet().adjustRange();
-
+//						if (!xValues.isEmpty() && !yValues.isEmpty()) {
+//							chart.getAxisSet().getXAxis(0).setRange(new Range(0, x[x.length - 1]));
+//							chart.getAxisSet().getYAxis(0).setRange(new Range(minY, maxY));
+//						} else {
+//							chart.getAxisSet().getXAxis(0).setRange(new Range(0, 1));
+//							chart.getAxisSet().getYAxis(0).setRange(new Range(0, 1));
+//						}
 						chart.redraw();
 					}
 
@@ -262,12 +414,21 @@ public class RTplotter extends TmfView {
 			@Override
 			public void handleFailure() {
 				// Request failed, not more data available
-				
+
 				super.handleFailure();
 			}
 		};
-		
+
 		trace.sendRequest(req);
+	}
+
+	@Override
+	public void dispose() {
+		while (job.getState() == Job.RUNNING) {
+
+		}
+		job.cancel();
+		super.dispose();
 	}
 
 	@Override
